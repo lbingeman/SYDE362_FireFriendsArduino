dc29cfe12432dc48f7411967920def8c485a880b
Laura Bingeman <l.bingema@icloud.com>
2018-03-31 21:42:39 -0400

    Add threshold values

--- server.js
+++ server.js
@@ -7,40 +7,148 @@ var apiURL = "https://syde362fireplansystem.azurewebsites.net"
 var sensorMapping = {
     "US1": {
         type: "Distance",
-        cells: [788,789,790,791,792]
+        cells: [788,789,790,791,792],
+        value: function(value) {
+            let threshold = 6;
+
+            if (value > threshold) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"US2": {
         type: "Distance",
-        cells: [400,440,480]
+        cells: [400,440,480],
+        value: function(value) {
+            let threshold = 6;
+
+            if (value > threshold) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"US3": {
         type: "Distance",
-        cells: [22,23,24,25,26,27,28,29,30,31]
+        cells: [22,23,24,25,26,27,28,29,30,31],
+        value: function(value) {
+            let threshold = 5;
+
+            if (threshold < value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"US4": {
         type: "Distance",
-        cells: [769,770,771,772,773]
+        cells: [769,770,771,772,773],
+        value: function(value) {
+            let threshold = 6;
+
+            if (threshold < value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"US5": {
         type: "Distance",
-        cells: [681, 641, 601]
+        cells: [681, 641, 601],
+        value: function(value) {
+            let threshold = 7;
+
+            if (threshold < value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"Temp1": {
         type: "Temperature",
-        cells: [440,480,520,560,600,479,519,559,518]
+        cells: [440,480,520,560,600,479,519,559,518],
+        value: function(value) {
+            let threshold = 40;
+
+            if (threshold < value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"Temp2": {
         type: "Temperature",
-        cells: [26,27,28,29,30,67,68,69,108]
+        cells: [26,27,28,29,30,67,68,69,70],
+        value: function(value) {
+            let threshold = 40;
+
+            if (threshold < value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"Temp3": {
         type: "Temperature",
-        cells: [778,779,780,781,782,739,740,741,700]
+        cells: [778,779,780,781,782,739,740,741,742],
+        value: function(value) {
+            let threshold = 40;
+
+            if (threshold < value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"Temp4": {
         type: "Temperature",
-        cells: [521,561,601,641,681,562,602,642,603]
+        cells: [521,561,601,641,681,562,602,642,603],
+        value: function(value) {
+            let threshold = 40;
+
+            if (threshold < value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"GAS4": {
         type: "Gas",
-        cells: [384,385,386,387,388,389,390, 424,425,426,427,428,429,430, 464,465,466,467,468,469,470]
+        cells: [384,385,386,387,388,389,390, 424,425,426,427,428,429,430, 464,465,466,467,468,469,470],
+        value: function(value) {
+            let threshold = 70;
+
+            if (threshold > value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"GAS2": {
         type: "Gas",
-        cells: [373,374,375,376,377,378,379,413,414,415,416,417,418,419,453,454,455,456,457,458,459]
+        cells: [373,374,375,376,377,378,379,413,414,415,416,417,418,419,453,454,455,456,457,458,459],
+        value: function(value) {
+            let threshold = 50;
+
+            if (threshold > value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"GAS3": {
         type: "Gas",
-        cells: [780,781,782,740,741,742]
+        cells: [780,781,782,740,741,742],
+        value: function(value) {
+            let threshold = 300;
+
+            if (threshold < value) {
+                return 1
+            } else {
+                return 0
+            }
+        }
     },"GAS1": {
         type: "Gas",
         cells: [ 1,
@@ -122,7 +230,16 @@ var sensorMapping = {
             287,
             288,
             289,
-            290 ]
+            290 ],
+            value: function(value) {
+                let threshold = 140;
+    
+                if (threshold > value) {
+                    return 1
+                } else {
+                    return 0
+                }
+            }
     }
 }
 
